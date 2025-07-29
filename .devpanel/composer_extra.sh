68e15eec8600450f62d40960e6dea2efeae886dc
Marcus Johansson <me@marcusmailbox.com>
2025-07-29 13:58:01 +0200

    Added field validation

--- .devpanel/composer_extra.sh
+++ .devpanel/composer_extra.sh
@@ -7,5 +7,5 @@ composer require 'drupal/ai_vdb_provider_postgres:^1.0@alpha'
 composer require 'drupal/ai_provider_litellm:^1.1@beta'
 composer require 'drupal/search_api:^1.38'
 composer require 'league/commonmark:^2.4'
-composer require 'drupal/field_validation^3.0@beta'
+composer require 'drupal/field_validation:^3.0@beta'
 composer require 'drupal/localgov_demo:^3.0' -W
